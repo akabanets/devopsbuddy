90c356974b8dcb46922601d7f3f580bb9a44299a
Andrey <kabanets@hotmail.com>
2017-06-03 17:13:35 +0300

    Load username/password of main user from one more property file

--- src/main/java/com/devopsbuddy/DevopsbuddyApplication.java
+++ src/main/java/com/devopsbuddy/DevopsbuddyApplication.java
@@ -8,6 +8,7 @@ import com.devopsbuddy.enums.PlansEnum;
 import com.devopsbuddy.enums.RolesEnum;
 import com.devopsbuddy.utils.UserUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,15 +21,23 @@ public class DevopsbuddyApplication implements CommandLineRunner {
 	@Autowired
 	private UserService userService;
 
+	@Value("${webmaster.username}")
+	private String webmasterUsername;
+	@Value("${webmaster.password}")
+	private String webmasterPassword;
+	@Value("${webmaster.email}")
+	private String webmasterEmail;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsbuddyApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		User user = UserUtils.createBasicUser("sample", "sample@email.com");
+		User user = UserUtils.createBasicUser(webmasterUsername, webmasterEmail);
+		user.setPassword(webmasterPassword);
 		Set<UserRole> userRoles = new HashSet<>();
-		userRoles.add(new UserRole(user, new Role((RolesEnum.BASIC))));
+		userRoles.add(new UserRole(user, new Role((RolesEnum.ADMIN))));
 		userService.createUser(user, PlansEnum.PRO, userRoles);
 	}
 }
